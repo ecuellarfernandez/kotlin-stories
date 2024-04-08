e22eb25f24287b210e34c5dc0d408f562b2b909d
ecuellarfernandez <ecuellarfernandez@gmail.com>
2024-04-08 14:06:16 -0400

    formatted

--- app/src/main/java/com/example/practicarecyclerview/ui/stories/StoriesActivity.kt
+++ app/src/main/java/com/example/practicarecyclerview/ui/stories/StoriesActivity.kt
@@ -3,17 +3,14 @@ package com.example.practicarecyclerview.ui.stories
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import com.example.practicarecyclerview.R
 import com.example.practicarecyclerview.databinding.ActivityStoriesBinding
 import com.example.practicarecyclerview.ui.adapters.PagerAdapter
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class StoriesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStoriesBinding
-    private val model: StoriesViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStoriesBinding.inflate(layoutInflater)
@@ -24,6 +21,7 @@ class StoriesActivity : AppCompatActivity() {
 
     private fun setupViewPager() {
         val imagenes = intent.getStringArrayListExtra("stories")
+
         Log.d("StoriesActivity", "setupViewPager: $imagenes")
         val adapter = PagerAdapter(this, imagenes)
         binding.viewPager.adapter = adapter
--- app/src/main/java/com/example/practicarecyclerview/ui/stories/StoriesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.practicarecyclerview.ui.stories
-
-import androidx.lifecycle.ViewModel
-
-class StoriesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
